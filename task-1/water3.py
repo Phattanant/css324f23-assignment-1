f711dd665a6b57f778dd66987ee993b7ee1c0987
Puchit Bunpleng <130487937+PuchitBunpleng@users.noreply.github.com>
2023-09-11 15:06:05 +0700

    Revised task1

--- task-1/water3.py
+++ task-1/water3.py
@@ -9,22 +9,6 @@ def is_goal(s):
 def successors(s):
     x, y, z = s
 
-    # empty
-    if x > 0:
-        yield ((0, y, z), x)
-    if y > 0:
-        yield ((x, 0, z), y)
-    if z > 0:
-        yield ((x, y, 0), z)
-
-    # fill
-    if x < 8:
-        yield ((8, y, z), 8 - x)
-    if y < 5:
-        yield ((x, 5, z), 5 - y)
-    if z < 3:
-        yield ((x, y, 3), 3 - z)
-
     # pour
     if x > 0 and y < 5:
         space = 5 - y
